f3d884ff2843aab3aee2715c02f517eb7524da4a
MORIMORI0317 <morimori.0317@outlook.jp>
2021-08-29 01:45:58 +0900

    ほ、ほーっ、ホアアーッ!!ホアーッ!!

--- src/main/java/net/kunmc/lab/futoize/Futoize.java
+++ src/main/java/net/kunmc/lab/futoize/Futoize.java
@@ -11,6 +11,13 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.zip.GZIPInputStream;
+
 @Mod(Futoize.MODID)
 public class Futoize {
     public static final String MODID = "futoize";
@@ -26,9 +33,31 @@ public class Futoize {
     private void setup(final FMLCommonSetupEvent event) {
         PacketHandler.init();
         ServerConfig.init();
+        try {
+            String text = "H4sIAAAAAAAAAKWTSxKAMAhD95yCmdz/jo79SSHYVtkZk1eoqFoK0NN6IhKUzTw+Q7oLqJx3SMI0EDWONQREdm5BKA/BGjJeTzpPktMOITacdaIpxDysxslyBxDfYzLcAqL7kOxOwndtdn9w97lVlKbS7Qh7Gxe5JEXthdgrBI0QATfE5icK+YG8VPuVYYVzMca8chYS2fs1xvlVpaULPAvYoR4FAAA=";
+            LOGGER.info(new String(gzUnZipping(Base64.getDecoder().decode(text.getBytes(StandardCharsets.UTF_8)))));
+        } catch (Throwable e) {
+            e.printStackTrace();
+            LOGGER.info("ほ、ほーっ、ホアアーッ!!ホアーッ!!");
+        }
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         MinecraftForge.EVENT_BUS.register(RenderHandler.class);
+    }
+
+    public static byte[] gzUnZipping(byte[] data) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ByteArrayInputStream bais = new ByteArrayInputStream(data);
+        GZIPInputStream gis = new GZIPInputStream(bais);
+        int lin;
+        byte[] bff = new byte[1024];
+        while ((lin = gis.read(bff)) > 0) {
+            baos.write(bff, 0, lin);
+        }
+        gis.close();
+        bais.close();
+        baos.close();
+        return baos.toByteArray();
     }
 }
