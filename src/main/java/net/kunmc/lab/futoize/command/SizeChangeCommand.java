751da5c9895cbb3614478503e550feb6a83d50fa
MORIMORI0317 <morimori.0317@outlook.jp>
2021-08-31 11:34:28 +0900

    add command log

--- src/main/java/net/kunmc/lab/futoize/command/SizeChangeCommand.java
+++ src/main/java/net/kunmc/lab/futoize/command/SizeChangeCommand.java
@@ -9,6 +9,7 @@ import net.kunmc.lab.futoize.packet.PacketHandler;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.server.ServerWorld;
 
 public class SizeChangeCommand {
@@ -22,6 +23,7 @@ public class SizeChangeCommand {
     }
 
     private static int setSize(CommandSource src, float size) {
+        src.sendSuccess(new StringTextComponent("巨大化倍率を").append(size + "倍にしました"), true);
         SizeChangerManager manager = SizeChangerManager.getInstance();
         manager.setCurrentSize(size);
         PacketHandler.sendSyncMessage();
@@ -30,6 +32,19 @@ public class SizeChangeCommand {
 
     private static int setEnable(CommandSource src, boolean enable) {
         SizeChangerManager manager = SizeChangerManager.getInstance();
+        if (enable && manager.isEnable()) {
+            src.sendFailure(new StringTextComponent("すでに巨大化は有効化されています"));
+            return 1;
+        } else if (!enable && !manager.isEnable()) {
+            src.sendFailure(new StringTextComponent("すでに巨大化は無効化されています"));
+            return 1;
+        }
+
+        if (enable)
+            src.sendSuccess(new StringTextComponent("巨大化を有効化しました"), true);
+        else
+            src.sendSuccess(new StringTextComponent("巨大化を無効化しました"), true);
+
         manager.setEnable(enable);
         for (ServerWorld level : src.getServer().getAllLevels()) {
             level.getEntities().forEach(Entity::refreshDimensions);
@@ -40,6 +55,21 @@ public class SizeChangeCommand {
 
     private static int setRandom(CommandSource src, boolean random) {
         SizeChangerManager manager = SizeChangerManager.getInstance();
+
+        if (random && manager.isRandom()) {
+            src.sendFailure(new StringTextComponent("すでにランダムは有効化されています"));
+            return 1;
+        } else if (!random && !manager.isRandom()) {
+            src.sendFailure(new StringTextComponent("すでにランダムは無効化されています"));
+            return 1;
+        }
+
+        if (random)
+            src.sendSuccess(new StringTextComponent("ランダムを有効化しました"), true);
+        else
+            src.sendSuccess(new StringTextComponent("ランダムを無効化しました"), true);
+
+
         manager.setRandom(random);
         for (ServerWorld level : src.getServer().getAllLevels()) {
             level.getEntities().forEach(Entity::refreshDimensions);
